e039c0771023bf1f5c840259309605420e06058c
zamami <s1413115@gmail.com>
2023-02-12 10:08:58 +0000

    bookモデルとコントローラーを作りました。

---
+++ app/controllers/user_book_controller.rb
@@ -0,0 +1,17 @@
+class UserBookController < ApplicationController
+  
+  # 本の一覧ページのアクション
+  def index
+
+  end
+
+  # 本の詳細ページのアクション
+  def show
+    # モデルは亀谷さんが作ってます
+    # @books = Book.all 
+  end
+
+
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_065352) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_100444) do
+  create_table "books", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
