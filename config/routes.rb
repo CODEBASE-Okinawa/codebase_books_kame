f2ab97db296c235b62a997af6671052e52906012
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-02-13 13:02:47 +0000

    Add_UserName Add_Seed_UserAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
  root to: 'books#index'
   devise_for :users
 
+  # ログアウト仮
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :books
 end
