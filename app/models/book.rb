65a63ba3acb109e0b6c31912520a290e30ae884c
zamami <s1413115@gmail.com>
2023-02-18 03:20:10 +0000

    画像アップロード機能、画像リサイズ済み

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+    has_one_attached :image do |attachable|
+        attachable.variant :display, resize_to_limit: [400, 400]
+      end
     has_many :lendings
     has_many :reservations
     has_many :users, through: :reservations
