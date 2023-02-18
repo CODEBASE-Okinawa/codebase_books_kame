cb76fa064e28fb09a12649cc2582f0b31e5dc33c
kameya <kameya@amanesha.com>
2023-02-18 08:57:51 +0000

    画像を変更しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,26 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.create!(name: "CodebaseAdmin",
-            email: "codebase@email.com",
-            password: "codebase",
-            status: 0)
+             email: "codebase@email.com",
+             password: "codebase",
+             status: 0)
 
 5.times do |n|
-    User.create!(name: "かめや#{n+1}",
-                email: "kameya#{n+1}@email.com",
-                password: "kameyatest")
+  User.create!(name: "かめや#{n + 1}",
+               email: "kameya#{n + 1}@email.com",
+               password: "kameyatest")
 end
-
 
 5.times do |n|
-    Book.create!(image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/image_01.png"), filename:"image_01.png"),
-                title: "Ruby#{n+1}",
-                author: "伊藤#{n+1}")
+  Book.create!(image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/book_01.jpg"), filename: "book_01.jpg"),
+               title: "Ruby#{n + 1}",
+               author: "伊藤#{n + 1}")
 end
 
-10.times do |n|
-    Reservation.create!(user_id:rand(4)+1,
-                        book_id:rand(4)+1,
-                        status:rand(2),
-                        reserved_start:Date.today,
-                        reserved_end:Date.today+7)
+10.times do |_n|
+  Reservation.create!(user_id: rand(1..4),
+                      book_id: rand(1..4),
+                      status: rand(2),
+                      reserved_start: Date.today,
+                      reserved_end: Date.today + 7)
 end
