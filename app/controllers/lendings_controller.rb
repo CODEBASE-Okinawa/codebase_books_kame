72d8c4e4f56d04d026193bdf08ffba4f24ec6366
zamami <s1413115@gmail.com>
2023-02-18 07:37:18 +0000

    貸出機能途中

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -8,4 +8,23 @@ class LendingsController < ApplicationController
 
     end
 
+    def new
+        @lending = Lending.new
+    end
+
+    def create
+        lending = Lending.new(lending_params)
+        if lending.save!
+            redirect_to books_url, notice: "貸出完了"
+        else
+            # render :new
+            redirect_to books_url
+        end
+    end
+
+
+    private
+    def lending_params
+        params.require(:lending).permit(:lend_date)
+    end
 end
