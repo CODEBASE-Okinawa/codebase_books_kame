63c8dec3f8f63f344689e008ddfb9ba67f8e3c61
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-02-19 14:27:01 +0000

    add_reservationscontroller #show #edit

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,5 +8,11 @@ class ReservationsController < ApplicationController
   end
 
   def show
+    @reservation_book = current_user.reservations.where(book_id:params[:id], status:0).first
+  end
+
+  def edit
+    current_user.reservations.where(book_id: params[:id], status: 0).first.update(status:1)
+    redirect_to reservations_path, notice:"予約を取り消しました"
   end
 end
