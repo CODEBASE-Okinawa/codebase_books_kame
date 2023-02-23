b68bdd11fb22e5ffc3c6d8914eb428a64a3cfd66
yo-gari <jankieblackeymonkey8188@gmail.com>
2023-02-23 07:08:32 +0000

    reservation_layout

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,6 +2,7 @@ class ReservationsController < ApplicationController
 
   def index
     @reservation_books = current_user.reservations.where(status: 0)
+    @lending_books = Lending.where(status: 0)
   end
 
   def reserve
